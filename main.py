e00cc4ad91d3ed85c48aec907f2acf2f48325ac9
whorps <wilwel070717@student.jenseneducation.se>
2024-03-04 13:02:48 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,72 @@
+import pygame
+
+class Cell:
+    def __init__(self, x, y, thickness):
+        self.x, self.y = x, y
+        self.thickness = thickness
+        self.walls = {'top': True, 'right': True, 'bottom': True, 'left': True}
+        self.visited = False
+
+
+class Maze:
+    def __init__(self, cols, rows):
+        self.cols = cols
+        self.rows = rows
+        self.thickness = 4
+        self.grid_cells = [Cell(col, row, self.thickness) for row in range(self.rows) for col in range(self.cols)]
+
+class player:
+    def __init__(self, x, y):
+        self.x = int(x)
+        self.y = int(y)
+        self.player_size = 10
+        self.rect = pygame.Rect(self.x, self.y, self.player_size, self.player_size)
+        self.color = (250, 120, 60)
+        self.velX = 0
+        self.velY = 0
+        self.left_pressed = False
+        self.right_pressed = False
+        self.up_pressed = False
+        self.down_pressed = False
+        self.speed = 4
+
+
+pygame.font.init()
+
+class Game:
+    def __init__(self, goal_cell, tile):
+        self.font = pygame.font.SysFont("impact", 35)
+        self.message_color = pygame.Color("darkorange")
+        self.goal_cell = goal_cell
+        self.tile = tile
+
+    # mål för spelare att nå
+    def finish(self, screen):
+        img_path = 'img/gate.png'
+        img = pygame.image.load(img_path)
+        img = pygame.transform.scale(img, (self.tile, self.tile))
+        screen.blit(img, (self.goal_cell.x * self.tile, self.goal_cell.y * self.tile))
+
+    # medelande vid vinst
+    def message(self):
+        msg = self.font.render('You Win!!', True, self.message_color)
+        return msg
+
+    # kollar så spelare nått målet
+    def is_game_over(self, player):
+        goal_cell_abs_x, goal_cell_abs_y = self.goal_cell.x * self.tile, self.goal_cell.y * self.tile
+        if player.x >= goal_cell_abs_x and player.y >= goal_cell_abs_y:
+            return True
+        else:
+            return False
+
+pygame.init()
+pygame.font.init()
+
+class Main():
+    def __init__(self, screen):
+        self.screen = screen
+        self.font = pygame.font.SysFont("impact", 30)
+        self.message_color = pygame.Color("cyan")
+        self.running = True
+        self.game_over = False
