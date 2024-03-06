cf29365b75f7513f0f978cb2f3a33be35d7f306e
whorps <wilwel070717@student.jenseneducation.se>
2024-03-06 17:40:04 +0100

    hja

--- main.py
+++ main.py
@@ -1,58 +1,50 @@
-import pygame
+# labyrint game
+import arcade
 
-class Cell:
-    def __init__(self, x, y, thickness):
-        self.x, self.y = x, y
-        self.thickness = thickness
-        self.walls = {'top': True, 'right': True, 'bottom': True, 'left': True}
-        self.visited = False
+SCREEN_WIDTH = 700
+SCREEN_HEIGHT = 700
+SCREEN_TITLE = "DodgeBird"
 
+PLAYER_SPEED = 6  # player speed
+# mazen som bakgrund
+class Game(arcade.Window):
+    def __init__(self, width, height, title):
+        super().__init__(width, height, title)
+        self.setup()
+        self.run = True  # Starts the game
+        # background
+        self.bg = arcade.load_texture("assets/maze.png")  # loading different
 
-class Maze:
-    def __init__(self, cols, rows):
-        self.cols = cols
-        self.rows = rows
-        self.thickness = 4
-        self.grid_cells = [Cell(col, row, self.thickness) for row in range(self.rows) for col in range(self.cols)]
-
-class player:
-    def __init__(self, x, y):
-        self.x = int(x)
-        self.y = int(y)
-        self.player_size = 10
-        self.rect = pygame.Rect(self.x, self.y, self.player_size, self.player_size)
-        self.color = (250, 120, 60)
-        self.velX = 0
-        self.velY = 0
+    def on_draw(self):
+        if self.run:
+            self.clear()
+            arcade.draw_texture_rectangle(SCREEN_WIDTH / 2,  # function for loading the background
+                                          SCREEN_HEIGHT / 2, SCREEN_WIDTH,
+                                          SCREEN_HEIGHT, self.bg)
+# spelarens movement
+    def movment(self):
+        # players movment
         self.left_pressed = False
         self.right_pressed = False
         self.up_pressed = False
         self.down_pressed = False
-        self.speed = 4
 
+# collision
 
-pygame.font.init()
-
-class Game:
-    def __init__(self, goal_cell, tile):
-        self.font = pygame.font.SysFont("impact", 35)
-        self.message_color = pygame.Color("darkorange")
-        self.goal_cell = goal_cell
-        self.tile = tile
-
-    # mål för spelare att nå
-    def finish(self, screen):
+# finish
+    # add goal point for player to reach
+    def add_goal_point(self, screen):
+        # adding gate for the goal point
         img_path = 'assets/gate.png'
-        img = pygame.image.load(img_path)
-        img = pygame.transform.scale(img, (self.tile, self.tile))
+        img = arcade.load_texture(img_path)
         screen.blit(img, (self.goal_cell.x * self.tile, self.goal_cell.y * self.tile))
 
-    # medelande vid vinst
+    # winning message
     def message(self):
         msg = self.font.render('You Win!!', True, self.message_color)
         return msg
 
-    # kollar så spelare nått målet
+    # checks if player reached the goal point
     def is_game_over(self, player):
         goal_cell_abs_x, goal_cell_abs_y = self.goal_cell.x * self.tile, self.goal_cell.y * self.tile
         if player.x >= goal_cell_abs_x and player.y >= goal_cell_abs_y:
@@ -60,13 +52,4 @@ class Game:
         else:
             return False
 
-pygame.init()
-pygame.font.init()
-
-class Main():
-    def __init__(self, screen):
-        self.screen = screen
-        self.font = pygame.font.SysFont("impact", 30)
-        self.message_color = pygame.Color("cyan")
-        self.running = True
-        self.game_over = False
+# main så spelet fungerar
